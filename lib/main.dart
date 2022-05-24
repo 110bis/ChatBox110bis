c3da6c072d0b6096ac1c6f1341659fb39541b66e
Quentin TREHEUX <quentin.treheux@epitech.eu>
2022-05-24 14:52:17 +0200

    [UPD] add animated gradient

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,23 @@ class LabState extends State<Lab> {
   List<String> answer = [];
   List<String> tmp = [];
 
+  List<Color> colorList = [
+    const Color.fromARGB(255, 255, 229, 82),
+    const Color.fromARGB(255, 228, 121, 74),
+    const Color.fromARGB(255, 173, 72, 71),
+  ];
+
+  bool started = false;
+
+  int index = 0;
+
+  Color bottomColor = const Color.fromARGB(255, 173, 72, 71);
+  Color centerColor = const Color.fromARGB(255, 228, 121, 74);
+  Color topColor = const Color.fromARGB(255, 255, 229, 82);
+
+  Alignment begin = Alignment.topRight;
+  Alignment end = Alignment.bottomLeft;
+
   splitData(String rep) {
     tmp = rep.split(';\n');
     for (var i = 0; i != tmp.length; ++i) {
@@ -52,6 +69,17 @@ class LabState extends State<Lab> {
       getData();
       return const Center(child: CircularProgressIndicator());
     } else {
+      if (!started) {
+        Future.delayed(const Duration(milliseconds: 10), () {
+          setState(() {
+            bottomColor = const Color.fromARGB(255, 255, 229, 82);
+            centerColor = const Color.fromARGB(255, 173, 72, 71);
+            topColor = const Color.fromARGB(255, 228, 121, 74);
+            index = index + 1;
+          });
+        });
+        started = true;
+      }
       return MaterialApp(
         theme: ThemeData(
           fontFamily: 'Marianne',
@@ -60,15 +88,47 @@ class LabState extends State<Lab> {
         title: Lab.name,
         home: Scaffold(
             appBar: const AppBarPage(),
-            body: Container(
-              decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topRight,
-                      end: Alignment.bottomLeft,
-                      colors: [
-                    Color.fromARGB(255, 255, 229, 82),
-                    Color.fromARGB(255, 173, 72, 71),
-                  ])),
+            body: AnimatedContainer(
+              duration: const Duration(seconds: 4),
+              onEnd: () {
+                setState(() {
+                  int index1 = 0;
+                  int index2 = 0;
+
+                  if (index == 2) {
+                    index = 0;
+                  } else {
+                    index = index + 1;
+                  }
+
+                  if (index == 0) {
+                    index1 = 1;
+                    index2 = 2;
+                  } else if (index == 1) {
+                    index1 = 2;
+                    index2 = 0;
+                  } else if (index == 2) {
+                    index1 = 0;
+                    index2 = 1;
+                  }
+
+                  // animate the color
+                  topColor = colorList[index];
+                  centerColor = colorList[index1];
+                  bottomColor = colorList[index2];
+                });
+              },
+              decoration: BoxDecoration(
+                gradient: LinearGradient(begin: begin, end: end, stops: const [
+                  0.2,
+                  0.5,
+                  0.75
+                ], colors: [
+                  topColor,
+                  centerColor,
+                  bottomColor,
+                ]),
+              ),
               child: ButtonList(ask: ask, answer: answer),
             )),
       );
