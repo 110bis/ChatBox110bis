b4d9003adfee226d50b9e173f8cffea1b59f9bf9
Quentin TREHEUX <quentin.treheux@epitech.eu>
2022-06-02 16:59:43 +0200

    [UPD] use google sheet

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-class Button extends StatefulWidget {
-  const Button({Key? key, required this.ask, required this.answer})
-      : super(key: key);
+import 'package:chatboxlab/global.dart' as global;
+import 'package:chatboxlab/models/faq.dart';
 
-  final String ask;
-  final String answer;
+class Button extends StatefulWidget {
+  const Button({Key? key, required this.faq}) : super(key: key);
+
+  final FAQ faq;
 
   @override
   ButtonState createState() => ButtonState();
@@ -19,8 +20,6 @@ class ButtonState extends State<Button> {
     return Column(children: [
       TextButton(
           style: ButtonStyle(
-            //backgroundColor: MaterialStateProperty.all<Color>(
-            //    const Color.fromARGB(255, 153, 214, 204)),
             backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
             foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
             minimumSize: MaterialStateProperty.all(
@@ -36,20 +35,18 @@ class ButtonState extends State<Button> {
                 return 5;
               },
             ),
-            //side: MaterialStateProperty.all<BorderSide>(const BorderSide(color: Colors.black, width: 0.5)),
             shape: MaterialStateProperty.all<BeveledRectangleBorder>(
                 const BeveledRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(5)))),
-            shadowColor: MaterialStateProperty.all<Color>(
-                const Color.fromARGB(255, 228, 121, 74)),
+            shadowColor: MaterialStateProperty.all<Color>(global.colorList[1]),
           ),
           child: Text(
-            widget.ask,
+            widget.faq.ask,
             textAlign: TextAlign.center,
             style: const TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
-              fontFamily: 'Marianne',
+              fontFamily: global.fontFamily,
             ),
           ),
           onPressed: () {
@@ -59,20 +56,19 @@ class ButtonState extends State<Button> {
           }),
       const SizedBox(height: 10),
       display
-          ? widget.answer.contains("/img/")
+          ? widget.faq.anwser.contains("/img/")
               ? const Text("img")
-              : widget.answer.contains(".png")
+              : widget.faq.anwser.contains(".png")
                   ? const Text("img")
                   : SizedBox(
                       width: MediaQuery.of(context).size.width * 0.95,
-                      //height: MediaQuery.of(context).size.height * 0.4,
                       child: Card(
                         color: Colors.white,
                         margin: const EdgeInsets.symmetric(
                             horizontal: 25, vertical: 0),
                         child: Container(
                             padding: const EdgeInsets.all(10),
-                            child: Text(widget.answer)),
+                            child: Text(widget.faq.anwser)),
                       ))
           : const SizedBox()
     ]);
