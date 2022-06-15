2daf305e32969ea5ed4bc1482a1c0436c5a015fe
Quentin TREHEUX <quentin.treheux@epitech.eu>
2022-06-15 17:37:24 +0200

    [UPD] change some details

--- lib/components/appbar.dart
+++ lib/components/appbar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:restart_app/restart_app.dart';
 
@@ -20,28 +21,26 @@ class AppBarState extends State<AppBarPage> {
   Widget build(BuildContext context) {
     return AppBar(
       title: const Text(
-<<<<<<< HEAD
         global.name,
-=======
-        Lab.name,
->>>>>>> 20-improve-design
         style: TextStyle(color: Colors.black, fontSize: 30),
       ),
       centerTitle: true,
       actions: [
-        IconButton(
-          onPressed: () {
-            Restart.restartApp();
-          },
-          color: Colors.black,
-          icon: const Icon(Icons.restart_alt_sharp),
-        ),
+        kIsWeb
+            ? const SizedBox()
+            : IconButton(
+                onPressed: () {
+                  Restart.restartApp();
+                },
+                color: Colors.black,
+                icon: const Icon(Icons.restart_alt_sharp),
+              ),
         IconButton(
           onPressed: () {
             showAboutDialog(
               context: context,
               applicationName: global.appName,
-              applicationVersion: global.version,
+              applicationVersion: "ver. " + global.version,
             );
           },
           color: Colors.black,
@@ -53,21 +52,9 @@ class AppBarState extends State<AppBarPage> {
             gradient: LinearGradient(
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
-<<<<<<< HEAD
                 colors: [
               orange,
               orange,
-=======
-                stops: [
-              0.2,
-              0.5,
-              0.75
-            ],
-                colors: [
-              Color.fromARGB(255, 255, 229, 82),
-              Color.fromARGB(255, 228, 121, 74),
-              Color.fromARGB(255, 173, 72, 71),
->>>>>>> 20-improve-design
             ])),
       ),
     );
